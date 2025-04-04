207938b710add02c34b7bbb5cb2d079d1f8b102f
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-04-04 05:44:32 -0400

    Update main.js

--- assign4/main.js
+++ assign4/main.js
@@ -40,8 +40,13 @@ function result() {
   const yItem = randomValueFromArray(insertY);
   const zItem = randomValueFromArray(insertZ);
 
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":inserty:", yItem);
+  newStory = newStory.replace(":insertz:", zItem);
+
   if(customName.value !== '') {
     const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
 
   }
 
