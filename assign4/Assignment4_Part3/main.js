2a6f7dfe4d101cdc9a0098e8593dbd808c272b3d
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-04-04 07:19:10 -0400

    Commit - 8

--- assign4/Assignment4_Part3/main.js
+++ assign4/Assignment4_Part3/main.js
@@ -63,7 +63,22 @@ class Ball {
       
         this.x += this.velX;
         this.y += this.velY;
-      }      
+      }
+      
+      collisionDetect() {
+        for (const ball of balls) {
+          if (this !== ball) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+      
+            if (distance < this.size + ball.size) {
+              ball.color = this.color = randomRGB();
+            }
+          }
+        }
+      }
+      
   }
 
   const balls = [];
@@ -91,6 +106,7 @@ function loop() {
     for (const ball of balls) {
       ball.draw();
       ball.update();
+      ball.collisionDetect();
     }
   
     requestAnimationFrame(loop);
