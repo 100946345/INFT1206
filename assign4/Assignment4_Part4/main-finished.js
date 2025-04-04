bf0c7bfa28b7f4fb21b8e6feafd38f691040f5f9
100946345 <manuelalexandre.balboaantunes@dcmail.ca>
2025-04-04 08:55:05 -0400

    Commit - 6

--- assign4/Assignment4_Part4/main-finished.js
+++ assign4/Assignment4_Part4/main-finished.js
@@ -116,7 +116,20 @@ class EvilCircle extends Shape {
             }
           });
     }
+
 }
+
+draw() {
+    ctx.beginPath();
+    lineWidth = 5
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+
+  
+
 
 
 const balls = [];
